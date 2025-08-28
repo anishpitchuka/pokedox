3de3ce59694c4fc61eeb6d77a9e2b81d774fe5c0
anishpitchuka <anishpitchuka8@gmail.com>
2025-08-28 18:22:33 +0000

    feat: help and exit at CLI

--- src/repl.ts
+++ src/repl.ts
@@ -1,30 +1,48 @@
-import { createInterface, Interface } from "readline";
+import { createInterface } from "readline";
+import { getCommands } from "./commands.js";
 
-export function startREPL(){
-    const rl = createInterface({
-        input: process.stdin,
-        output: process.stdout,
-        prompt: "pokedox >",
-    });
+export function startREPL() {
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "pokedex > ",
+  });
+
+  rl.prompt();
+
+  rl.on("line", async (input) => {
+    const words = cleanInput(input);
+    if (words.length === 0) {
+      rl.prompt();
+      return;
+    }
+
+    const commandName = words[0];
+
+    const commands = getCommands();
+    const cmd = commands[commandName];
+    if (!cmd) {
+      console.log(
+        `Unknown command: "${commandName}". Type "help" for a list of commands.`,
+      );
+      rl.prompt();
+      return;
+    }
+
+    try {
+      cmd.callback(commands);
+    } catch (e) {
+      console.log(e);
+    }
 
     rl.prompt();
-
-    rl.on("line", async (input) => {
-        const words = cleanInput(input);
-        if(words.length === 0){
-            rl.prompt();
-            return;
-        }
-        const firstWord = words[0];
-        console.log(`Your command was: ${firstWord}`);
-        rl.prompt();
-    })
+  });
 }
 
-export function cleanInput(input: string) : string[] {
-    return input
-        .trim()
-        .toLocaleLowerCase()
-        .split(" ")
-        .filter((word) => word !== "");
+export function cleanInput(input: string): string[] {
+  return input
+    .toLowerCase()
+    .trim()
+    .split(" ")
+    .filter((word) => word !== "");
 }
