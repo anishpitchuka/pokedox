1d3c5adb980d22799e36539ea16ecd3d9c23ba21
anishpitchuka <anishpitchuka8@gmail.com>
2025-08-28 15:38:14 +0000

    feat: add REPL

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,26 @@
+import { createInterface, Interface } from "readline";
+
+export function startREPL(){
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: "pokedox >",
+    });
+
+    rl.prompt();
+
+    rl.on("line", async (input) => {
+        const words = cleanInput(input);
+        if(words.length === 0){
+            rl.prompt();
+            return;
+        }
+        const firstWord = words[0];
+        console.log(`Your command was: ${firstWord}`);
+        rl.prompt();
+    })
+}
+
 export function cleanInput(input: string) : string[] {
     return input
         .trim()
